beadd3992051d8fcce29c305c4daad0634ec8b74
Ubuntu <ubuntu@ip-172-31-90-30.ec2.internal>
2023-07-31 13:45:32 +0000

    this script file

---
+++ shell_script_challenges/day_01_task.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#shell script is filer containing a series of comments it has been directely on command line
+# in the shell srcipt start from #!/bin/bash this is called sebang is it first step to write any shell script
+echo "welcome to shell script tws7daychallenges this is day one task! "
+
+#In the shell srcipt there are two types of variables
+#1. internal variable 2. user defind  variables
+
+echo 'BASH='$BASH
+echo 'BASH_VERSION='$BASH_VERSION
+HOST_NAME="ubuntu"
+echo "host_name is $HOST_NAME"
+
+#user defind varibale
+
+name="akash"
+echo "My name is $name"
+learning="DevOps"
+echo "I'm Learning $learning "
+
+
